1937e9e4bb9f84e61597f02a5f2f153d2ef73196
jeff.42(박제헌) <jeff.42@kakaocorp.com>
2022-07-03 15:34:48 +0900

    SEOUL-1 stockDaoImpl 기본 메서드 추가

--- seoul-commons-dao/src/main/kotlin/dao/jdbc/StockDaoImpl.kt
+++ seoul-commons-dao/src/main/kotlin/dao/jdbc/StockDaoImpl.kt
@@ -2,6 +2,7 @@ package dao.jdbc
 
 import dao.StockDao
 import model.entity.StockEntity
+import model.entity.UserEntity
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.jdbc.core.JdbcTemplate
@@ -25,7 +26,7 @@ class StockDaoImpl(@Autowired val jdbcTemplate: JdbcTemplate) : StockDao {
         val findByIdSql = "select ${StockEntity.COLUMN_ID}, " +
             "${StockEntity.COLUMN_FULLNAME}, " +
             "${StockEntity.COLUMN_TICKER}, " +
-            "${StockEntity.COLUMN_LASTDAY_PRICE}, " +
+            "${StockEntity.COLUMN_LASTDAY_PRICE}" +
             "where id = ?"
         return jdbcTemplate.queryForObject(findByIdSql, mapper, stockId)
     }
@@ -34,8 +35,37 @@ class StockDaoImpl(@Autowired val jdbcTemplate: JdbcTemplate) : StockDao {
         val findByTickerSql = "select ${StockEntity.COLUMN_ID}, " +
             "${StockEntity.COLUMN_FULLNAME}, " +
             "${StockEntity.COLUMN_TICKER}, " +
-            "${StockEntity.COLUMN_LASTDAY_PRICE}, " +
+            "${StockEntity.COLUMN_LASTDAY_PRICE} " +
             "where ticker = ?"
         return jdbcTemplate.queryForObject(findByTickerSql, mapper, ticker)
+    }
+
+    override fun delete(stockId: Long): Int {
+        val deleteQuery = "delete from stocks where id = ?"
+        return jdbcTemplate.update(deleteQuery, stockId)
+    }
+
+    override fun save(entity: StockEntity): Int {
+        val saveQuery = "insert into stocks (" +
+            "${StockEntity.COLUMN_FULLNAME}, " +
+            "${StockEntity.COLUMN_TICKER}, " +
+            "${StockEntity.COLUMN_LASTDAY_PRICE} " +
+            "values(?, ?, ?)"
+        return jdbcTemplate.update(saveQuery, entity.fullname, entity.ticker, entity.lastDayPrice)
+    }
+
+    override fun update(entity: StockEntity): Int {
+        val updateQuery = "update stocks set " +
+            "${StockEntity.COLUMN_FULLNAME}=?, " +
+            "${StockEntity.COLUMN_TICKER}=?, " +
+            "${StockEntity.COLUMN_LASTDAY_PRICE}=?, " +
+            "where id=?"
+        return jdbcTemplate.update(
+            updateQuery,
+            entity.fullname,
+            entity.ticker,
+            entity.lastDayPrice,
+            entity.id
+        )
     }
 }
